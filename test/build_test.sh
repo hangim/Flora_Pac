d57f2d8199fa86ab7c702b4f8819969f0dc09527
hang <git@hang.im>
2015-08-15 20:44:27 +0800

    move

---
+++ test/build_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../flora_pac -x 'SOCKS5 127.0.0.1:8964; SOCKS 127.0.0.1:8964; DIRECT'
--- test/performance_test.sh
+++ test/performance_test.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 if [ -n "$1" ]
 then
-    cat ./pac_utils.js ./$1 ./performance_test.js > /tmp/test.js
+    cat ./pac_utils.js $1 ./performance_test.js > /tmp/test.js
     node /tmp/test.js
 else
-    echo "usage: ./test.sh flora_pac.pac"
+    echo "usage: $0 ./flora_pac.pac"
 fi
